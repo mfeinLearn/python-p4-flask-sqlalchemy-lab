7f52fedc15c4d052ae0cddf5ea24a5e2b0c2cc26
Malcome Fein <mfein90@gmail.com>
2022-12-21 09:48:56 -0500

    Done.

--- server/app.py
+++ server/app.py
@@ -13,22 +13,70 @@ migrate = Migrate(app, db)
 
 db.init_app(app)
 
+# animal_by_id, zookeeper_by_id, and enclosure_by_id
+
 @app.route('/')
 def home():
     return '<h1>Zoo app</h1>'
 
 @app.route('/animal/<int:id>')
 def animal_by_id(id):
-    return ''
+    animal = Animal.query.filter(Animal.id == id).first()
+
+    if not animal:
+        response_body = '<h1>404 animal not found</h1>'
+        response = make_response(response_body, 404)
+        return response
+
+    response_body = f'''
+            <ul>ID: {animal.id}</ul>
+            <ul>Name: {animal.name}</ul>
+            <ul>Species: {animal.species}</ul>
+            <ul>Zookeeper: {animal.zookeeper.name}</ul>
+            <ul>Enclosure: {animal.enclosure.environment}</ul>
+    '''
+
+    response = make_response(response_body, 200)
+
+    return response
+
+
 
 @app.route('/zookeeper/<int:id>')
 def zookeeper_by_id(id):
-    return ''
+    zookeeper = Zookeeper.query.filter(Zookeeper.id == id).first()
+    if not zookeeper:
+        response_body = '<h1>404 zookeeper not found</h1>'
+        response = make_response(response_body, 404)
+        return response
+    
+    response_body = f''
+    response_body += f'<ul>ID: {zookeeper.id}</ul>'
+    response_body += f'<ul>Name: {zookeeper.name}</ul>'
+    response_body += f'<ul>Birthday: {zookeeper.birthday}</ul>'
+
+    for animal in zookeeper.animals:
+        response_body += f'<ul>Animal: {animal.name}</ul>'
+
+    return make_response(response_body)
+
 
 @app.route('/enclosure/<int:id>')
 def enclosure_by_id(id):
-    return ''
+    enclosure = Enclosure.query.filter(Enclosure.id == id).first()
+    response_body = f''
+    response_body += f'<ul>ID: {enclosure.id}</ul>'
+    response_body += f'<ul>Environment: {enclosure.environment}</ul>'
+    response_body += f'<ul>Open to Visitors: {enclosure.open_to_visitors}</ul>'
+
+    for animal in enclosure.animals:
+        response_body += f'<ul>Animal: {animal.name}</ul>'
+
+    return make_response(response_body)
+
 
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
+
+
--- server/models.py
+++ server/models.py
@@ -1,18 +1,33 @@
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.orm import relationship, backref
 
 db = SQLAlchemy()
 
 class Zookeeper(db.Model):
+    # [x]name, a [x]birthday, and a list of [x]animals that they take care of.
     __tablename__ = 'zookeepers'
 
     id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String)
+    birthday = db.Column(db.DateTime)
+    animals = db.relationship('Animal', backref=backref('zookeeper'))
 
+#db.session.query(Animal).count()
 class Enclosure(db.Model):
+    #  [x]environment (grass, sand, or water), an [x]open_to_visitors boolean, and a [x]list of animals.
     __tablename__ = 'enclosures'
 
     id = db.Column(db.Integer, primary_key=True)
+    environment = db.Column(db.String)
+    open_to_visitors = db.Column(db.Boolean, default=False)
+    animals = db.relationship('Animal', backref=backref('enclosure'))
 
 class Animal(db.Model):
+    # [x]name, a [x]species, a [x]zookeeper, and an [x]enclosure
     __tablename__ = 'animals'
 
     id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String)
+    species = db.Column(db.String)
+    zookeeper_id = db.Column(db.Integer, db.ForeignKey('zookeepers.id'))
+    enclosure_id = db.Column(db.Integer, db.ForeignKey('enclosures.id'))
